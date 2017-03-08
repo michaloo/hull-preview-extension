e824d3b0e734b3e1f1035e1e7523a96db4bb922c
Thomas Kgaevski <thomas@hull.io>
2017-03-08 17:48:59 +0100

    store options in local storage on change

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,7 +1,18 @@
-function onSwitch() {
-    console.log("hello");
+function onSwitch(event) {
+	var switchChecked = event.target.checked;
+	chrome.storage.sync.set({ switch: switchChecked }, function() {
+		console.debug(`Options saved: switch=${switchChecked}`);
+	});
+}
+
+function onTokenChange(event) {
+	var token = event.target.value;
+	chrome.storage.sync.set({ token: token }, function() {
+		console.debug(`Options saved: token=${token}`);
+	});
 }
 
 window.onload = function() {
-	document.getElementById('switch').addEventListener('change', onSwitch); 
+	document.getElementById("token").addEventListener("change", onTokenChange);
+	document.getElementById("switch").addEventListener("change", onSwitch); 
 }
