aa978fd4282f127c50f68de68e7612ca88030b6e
Michal Raczka <michal@raczka.me>
2017-03-08 17:39:16 +0100

    Adds background script and resize body

--- src/scripts/inject-iframe.js
+++ src/scripts/inject-iframe.js
@@ -3,4 +3,5 @@ function injectIframe(src) {
   iframe.src = src;
   iframe.setAttribute("style", "position: fixed;top: 0;right: 0;bottom: 0;width: 20%;height: 100%;background: white;z-index:9999;border: none;border-left: 1px solid black;")
   document.body.appendChild(iframe);
+  document.body.style.width = "80%";
 }
