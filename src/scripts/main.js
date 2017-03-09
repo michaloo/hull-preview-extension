b231efdf6240e7619d2faedce1a3b49faa5bbaea
Thomas Kgaevski <thomas@hull.io>
2017-03-09 12:50:02 +0100

    let init function return a promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,51 @@
 var loaded = false;
+//var iframe = null;
 
 function init() {
-  Promise.all([
+  return Promise.all([
   	getSwitch(),
   	getToken(),
   	getIdent(),
   	getConnectorUrl()
-  ]).then(function(input) {
+  ]).then(function(input, err) {
   	var switchEnabled = input[0];
   	var token = input[1];
   	var ident = input[2];
   	var connectorUrl = input[3];
 
   	if (!switchEnabled || !token || !connectorUrl || JSON.stringify(ident) === "{}") {
+  		/*
+  		if (iframe) {
+  			hideIframe(iframe);
+  			iframe = null;
+  		}
+  		*/
     	return false;
   	}
 
   	ident = encodeIdent(ident);
 
-  	injectIframe(connectorUrl + "/preview?hullToken=" + token + "&ident=" + ident);
+	/*
+  	if (iframe) {
+  		hideIframe(iframe);
+  		iframe = null;
+  	}
+  	*/
+  	iframe = injectIframe(connectorUrl + "/preview?hullToken=" + token + "&ident=" + ident);
 
   	return true;
   })
 }
 
 function checkInit(count) {
+  console.log(loaded, count);
   if (loaded || count > 20) {
     return;
   }
-  loaded = init();
-  setTimeout(checkInit.bind(undefined, count + 1), 200);
+  init().then(function(res, err) {
+  	loaded = res;
+  	setTimeout(checkInit.bind(undefined, count + 1), 200);
+  });
 }
 
-checkInit();
+checkInit(1);
