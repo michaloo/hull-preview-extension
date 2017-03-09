d520259a787a2a020e24f5ca407e8b1d73fcef70
Thomas Kgaevski <thomas@hull.io>
2017-03-09 10:39:38 +0100

    get token and host from chrome storage

--- src/scripts/get-connector-url.js
+++ src/scripts/get-connector-url.js
@@ -1,3 +1,8 @@
 function getConnectorUrl() {
-  return "https://hull-preview.eu.ngrok.io";
+	return "https://hull-preview.eu.ngrok.io";
+  	return Promise(function(resolve, reject) {
+  		chrome.storage.sync.get("host", function(items) {
+  			resolve(items.host);
+  		}
+  	});
 }
